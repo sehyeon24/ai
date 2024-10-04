f4cf5197fef74b67b968b529c48010cac704b6b3
sehyeon24 <sehyeon_24@naver.com>
2024-10-04 18:04:20 +0900

    VisualStudio

---
+++ source/03_HTML-CSS/ch01/2.js
@@ -0,0 +1,7 @@
+﻿// 2.js
+// 다른이름으로 저장(utf-8 인코딩방법으로 저장)
+/* 동적인 부분(JavaScript */
+name = prompt("이름은 ?", "홍길동");  // 취소를 클릭하면 'null'이 return 됨
+if (name != "null" && name != "") {
+    document.write(name + "님 반갑습니다.<br>");
+}
