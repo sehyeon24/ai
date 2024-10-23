f72d92ddcfa2a885fb251c629a6206494498e67f
sehyeon24 <sehyeon_24@naver.com>
2024-10-23 17:59:43 +0900

    jsp

---
+++ source/05_jsp/ch04_servlet/src/main/java/com/lec/lect/Ex2Params.java
@@ -0,0 +1,48 @@
+package com.lec.lect;
+
+import jakarta.servlet.ServletException;
+import jakarta.servlet.annotation.WebServlet;
+import jakarta.servlet.http.HttpServlet;
+import jakarta.servlet.http.HttpServletRequest;
+import jakarta.servlet.http.HttpServletResponse;
+import java.io.IOException;
+import java.io.PrintWriter;
+import java.util.Arrays;
+@WebServlet("/ex2")
+public class Ex2Params extends HttpServlet {
+	private static final long serialVersionUID = 1L;
+       
+	protected void doGet(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
+		System.out.println("doGet()호출");
+		actionDo(request, response);
+	}
+
+	protected void doPost(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
+		System.out.println("doPost()호출");
+		actionDo(request, response);
+	}
+
+	private void actionDo(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
+		// 복수 개의 menu, rest 파라미터, 1개의 gender 파라미터 
+		String gender = request.getParameter("gender");	// null or 1개 / 같은 이름의 파라미터가 없을 때
+		String[] menu = request.getParameterValues("menu");	// 같은 이름의 파라미터가 전달될 때 사용됨
+		String[] rest = request.getParameterValues("rest");
+		response.setContentType("text/html; charset=UTF-8");
+		PrintWriter out = response.getWriter();	// 스트림 생성
+		if(menu != null) {
+			out.println("<h2>선택한 메뉴는 : ");
+			for(String m : menu) {	// 선택한 메뉴를 받을 변수 m
+				out.println(m + " ");
+			}
+			out.println("입니다</h2>");
+		}else {
+			out.println("<h2>선택한 메뉴가 없습니다</h2>");
+		}
+		// out.println("<h2>선택한 메뉴는 " + Arrays.toString(menu) + "입니다.</h2>"); 위 if문과 같은 출력
+		out.println("<h2>선택한 식당은 " + Arrays.toString(rest) + "입니다.</h2>");
+		String genderOut = gender == null ? "-" : gender.equals("m") ? "남자" : "여자";	// 삼항연산자(2번)
+		out.println("<h2>성별은 " + genderOut + "입니다.</h2>");
+		out.close();		
+	}
+
+}
---
+++ source/05_jsp/ch04_servlet/src/main/java/com/lec/quiz/Quiz2.java
@@ -0,0 +1,32 @@
+package com.lec.quiz;
+
+import jakarta.servlet.ServletException;
+import jakarta.servlet.annotation.WebServlet;
+import jakarta.servlet.http.HttpServlet;
+import jakarta.servlet.http.HttpServletRequest;
+import jakarta.servlet.http.HttpServletResponse;
+import java.io.IOException;
+import java.io.PrintWriter;
+import java.util.Arrays;
+@WebServlet("/Quiz2")
+public class Quiz2 extends HttpServlet {
+	private static final long serialVersionUID = 1L;
+      
+	protected void doGet(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
+		int su = Integer.parseInt(request.getParameter("su"));
+		int sum = 0;
+		for(int i = 1; i <= su; i++) {
+			sum += i;
+		}
+		System.out.println("1부터 "+su+"까지 누적합은 " + sum);
+		response.setContentType("text/html;charset=utf-8");
+		PrintWriter out = response.getWriter();
+		out.println("<h2>1부터 "+su+"까지 누적합은 "+sum+"</h2>");
+		out.close();		
+	}
+
+	protected void doPost(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
+		doGet(request, response);
+	}
+
+}
