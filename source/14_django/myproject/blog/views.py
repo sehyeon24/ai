54e82db858984c81bce4911e378ce3b87308c66e
sehyeon24 <sehyeon_24@naver.com>
2025-02-18 11:25:13 +0900

    django

---
+++ source/14_django/myproject/blog/views.py
@@ -0,0 +1,15 @@
+from django.http import HttpResponse
+from django.shortcuts import render
+from .models import Post
+
+# Create your views here.
+
+def index(request):
+    post_list = Post.objects.all()
+    output = '<br>'.join([post.__str__() for post in post_list])
+    print(output)
+    return HttpResponse('<h1>Welcome page</h1>' + output)
+
+def detail(request, post_id):
+    post = Post.objects.get(pk=post_id)
+    return HttpResponse(post.__str__())
