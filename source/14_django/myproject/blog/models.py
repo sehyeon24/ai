bd96d7e10fe9daaca99198e77a8337f5bbc03d91
sehyeon24 <sehyeon_24@naver.com>
2025-02-20 11:19:48 +0900

    django

--- source/14_django/myproject/blog/models.py
+++ source/14_django/myproject/blog/models.py
@@ -1,14 +1,34 @@
 from django.db import models
+import re
+from django.forms import ValidationError
 
 # Create your models here.
 
+REGION_CHOICE = (
+    ("Europe", "유럽"),
+    ("Asia", "아시아"),
+    ("Africa", "아프리카"),
+    ("Oceania", "오세아니아"),
+    ("America", "아메리카"),
+)
+def lnglat_validator(value):
+    if not re.match(r'(\d+\.?\d*),(\d+\.?\d*)', value):
+        raise ValidationError('Invalid LngLat. ex:38,125')
+
 class Post(models.Model):  # 테이블명 : blog_post
     # id = models.AutoField(primary_key=True)  # PK가 없을 경우 자동 생성
-    title     = models.CharField(max_length=100)   # 최대 길이 반드시 지정 VARCHAR 타입
-    content   = models.TextField()  # 최대 길이 제한 없음 CLOB(oracle/2GB), TEXT 타입
+    title     = models.CharField(verbose_name='제목', max_length=100)   # 최대 길이 반드시 지정 VARCHAR 타입
+    content   = models.TextField(verbose_name='본문')  # 최대 길이 제한 없음 CLOB(oracle/2GB), TEXT 타입
     create_at = models.DateField(auto_now_add=True)
     update_at = models.DateTimeField(auto_now=True)
+    region    = models.CharField(verbose_name='지역', max_length=20, choices=REGION_CHOICE, default='Asia')
+    lnglat    = models.CharField(verbose_name='경,위도', max_length=100,
+                                 blank=True, null=True,
+                                 help_text='경도,위도 포맷', validators=[lnglat_validator]) # 38.5,127.4125 / 38,127
+    url       = models.URLField(blank=True, null=True)
+
     def __str__(self):
-        return '제목:{} - {}작성, 최종수정일:{:%Y-%m-%d %p %I:%M:%S}'.format(self.title, self.create_at, self.update_at)
+        return '제목 : {} - {}작성, 최종수정일 : {:%Y-%m-%d %p %I:%M:%S}'.format(self.title, self.create_at, self.update_at)
+
     class Meta:
         ordering = ['-update_at']  # update_at 기준 내림차순
--- source/14_django/myproject/blog/tests.py
+++ source/14_django/myproject/blog/tests.py
@@ -1,3 +1,14 @@
 from django.test import TestCase
+import re
 
 # Create your tests here.
+
+# 38.5,127.4125 / 38,127
+# value = input('경도,위도는?')  # \d:숫자, +:1번이상, \.?:.이 올수도 안 올수도 있음 *:0번 이상
+# re_result = re.match(r'(\d+\.?\d*),(\d+\.?\d*)', value)
+# if not re_result:
+#     print('경도,위도 포맷으로 입력하지 않았습니다.')
+
+value = input('경도,위도는?')
+if not re.match(r'(\d+\.?\d*),(\d+\.?\d*)', value):
+    print('경도,위도 포맷으로 입력하지 않았습니다.')
