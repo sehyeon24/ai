c90cee520d28ae04162ba9d10128efd863b36849
sehyeon24 <sehyeon_24@naver.com>
2025-02-25 11:46:43 +0900

    book

---
+++ source/14_django/myproject/book/forms.py
@@ -0,0 +1,38 @@
+from django import forms
+from django.core.validators import MinLengthValidator
+from django.core.validators import MinValueValidator
+from django.core.validators import MaxValueValidator
+from .models import Book
+
+class BookForm(forms.Form):
+    title     = forms.CharField(label='책제목')
+    author    = forms.CharField(label='글쓴이', validators=[MinLengthValidator(3)])
+    publisher = forms.CharField(label='출판사', required=False)
+    sales     = forms.IntegerField(initial=1000, validators=[MinValueValidator(0), MaxValueValidator(1000000)])
+    def save(self, commit=False):
+        # book = Book(title = self.cleaned_data['title'],
+        #             author = self.cleaned_data['author'],
+        #             publisher = self.cleaned_data['publisher'],
+        #             sales = self.cleaned_data['sales'])
+        book = Book(**self.cleaned_data)
+        if commit:
+            book.save()
+        return book
+
+class BookModelForm(forms.ModelForm):  # ModelForm 내 save() 포함
+    class Meta:
+        model = Book
+        fields = ['title', 'author', 'publisher', 'sales']
+        # fields = '__all__'
+    def clean_author(self):
+        author = self.cleaned_data['author']
+        if author:
+            if len(author) < 3:
+                raise forms.ValidationError('저자 입력 시 3글자 이상')
+        return author
+    # def check_exist(self, title, author):
+    #     return Book.objects.filter(title=title, author=author).exists()
+    # def clean(self):
+    #     cleaned_data = super().clean()  # self.cleaned_data['title']
+    #     if self.check_exist(cleaned_data.get('title'), cleaned_data.get('author')):
+    #         return forms.ValidationError('이미 등록된 책입니다.')
--- source/14_django/myproject/book/views.py
+++ source/14_django/myproject/book/views.py
@@ -1,3 +1,34 @@
-from django.shortcuts import render
+from django.shortcuts import render, redirect
+
+from .forms import BookForm, BookModelForm
+from .models import Book
+from django.views.generic import ListView
+from django.views.generic import CreateView
+from django.views.generic import UpdateView
+from django.views.generic import DeleteView
 
 # Create your views here.
+
+book_list = ListView.as_view(model=Book)
+# def book_list(request):
+#     return render(request, 'book/book_list.html', {'book_list':Book.objects.all()})
+
+def book_new(request):
+    if request.method == 'POST':
+        # request.POST의 파라미터 값을 book으로 save()
+        form = BookModelForm(request.POST, request.FILES)
+        if form.is_valid():  # validation 유효성 검사
+            book = Book(**form.cleaned_data)
+            book.ip = request.META['REMOTE_ADDR']  # 요청한 client의 ip
+            book.save()
+            return redirect('book:list')
+    else:
+        # form = BookForm()
+        form = BookModelForm()
+    return render(request, 'book/book_form.html', {'form':form})
+
+def book_edit(request):
+    pass
+
+def book_delete(request):
+    pass
